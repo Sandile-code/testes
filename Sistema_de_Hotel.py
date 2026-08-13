e907b4855430884cac110215ad8a18e908552155
Sandile-code <sandile.assuine@gmail.com>
2026-08-13 16:12:12 -0400

    improving

--- Sistema_de_Hotel.py
+++ Sistema_de_Hotel.py
@@ -1,4 +1,24 @@
 from abc import ABC, abstractmethod
+class Hotel:
+    def __init__(self):
+        self.quartos = []
+
+    def reservar_quarto (self):
+        if not self.quartos:
+            print("Ainda não existem quartos cadastrados.")
+            return
+
+        
+        
+
+
+
+class QuartoIndisponivelError(Exception):
+    pass
+
+class QuartoNaoEncontradoError(Exception):
+    pass
+
 class Quarto(ABC):
     def __init__(self, numero, preco_diaria, disponivel):
         self.numero = numero
@@ -14,6 +34,7 @@ class Quarto(ABC):
     def liberar(self):    
         if self._disponivel == False:
             self._disponivel == True
+            return "Quarto liberado."
         
     @abstractmethod
     def calcular_custos(self, dias):
@@ -23,3 +44,58 @@ class Quarto(ABC):
         print(f"Quarto {self.numero}")
         print(f"Diária: {self.preco_diaria}")
         print(f"Disponibilidade: {self._disponivel}")
+
+class QuartoSimples(Quarto):
+    def __init__(self, numero, preco_diaria, disponivel):
+        super().__init__(numero, preco_diaria, disponivel)
+       
+
+    def calcular_custos(self, dias):
+        return self.preco_diaria * dias
+
+    def mostrar_dados(self):
+        return super().mostrar_dados()    
+
+class QuartoLuxo(Quarto):
+    def __init__(self, _numero, preco_diaria, disponivel):
+        super().__init__( preco_diaria, disponivel)
+        self._numero = _numero
+
+    @property
+    def numero(self):
+        return self._numero
+    @numero.setter
+    def numero (self, valor):
+        if valor <= 10:
+            print("Número de Quarto inválido")
+        else:
+            self._numero = valor
+
+    def calcular_custos(self, dias):
+        return self.preco_diaria * dias * 1.2
+
+    def mostrar_dados(self):
+        return super().mostrar_dados()
+
+class Suite(Quarto):
+    def __init__(self, _codigo, preco_diaria, disponivel):
+        super().__init__( preco_diaria, disponivel)
+        self._codigo = _codigo
+
+    @property
+    def codigo (self):
+        return self._codigo
+    @codigo.setter
+    def codigo (self, valor):
+        if valor > 10 or valor < 0:
+            print("Número de quarto inválido.")
+        else:
+            self._codigo = valor
+
+
+    def calcular_custos(self, dias):
+        return self.preco_diaria * dias * 4.5
+
+    def mostrar_dados(self):
+        return super().mostrar_dados()
+
